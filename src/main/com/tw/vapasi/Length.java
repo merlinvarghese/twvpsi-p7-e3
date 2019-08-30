f38ad66b3038e9c3d57d2c86f1d81920c3bf5feb
Nidhi Vashishth <vashishth.nidhi@gmail.com>
2019-08-30 21:46:27 +0530

    [Nidhi,Ashima] Refactor. factory methods,enum,equals()

--- src/main/com/tw/vapasi/Length.java
+++ src/main/com/tw/vapasi/Length.java
@@ -4,42 +4,33 @@ import java.util.Objects;
 
 //understands measurements
 class Length {
-    private final static String CM = "cm";
-    private final static String METER = "m";
-    private final static String KILOMETER = "km";
     private final static int MULTIPLIER_CM_METER = 100;
     private final static int MULTIPLIER_CM_KILOMETER = 100000;
 
-    private final int value;
-    private final String unit;
+    private final double magnitude;
+    private final Unit unit;
 
-    Length(int value, String unit) {
-        this.value = value;
+    enum Unit {
+        CM,
+        METER,
+        KM
+    }
+
+    private Length(double magnitude, Unit unit) {
+        this.magnitude = magnitude;
         this.unit = unit;
     }
 
-    private boolean compareTo(Length other) {
-        Length newLengthThis = convertToCentiMeter(this);
-        Length newLengthOther = convertToCentiMeter(other);
-        return (newLengthThis.value == newLengthOther.value);
+    static Length cm(double magnitude) {
+        return new Length(magnitude, Unit.CM);
     }
 
-    // Converts all length objects to corresponding cm object
-    private Length convertToCentiMeter(Length length) {
-        Length convertedLength;
-        int value = 0;
-        String unit = null;
-        if (length.unit.equals(CM)) return length;
-        if (length.unit.equals(METER)) {
-            value = length.value * MULTIPLIER_CM_METER;
-            unit = CM;
-        }
-        if (length.unit.equals(KILOMETER)) {
-            value = length.value * MULTIPLIER_CM_KILOMETER;
-            unit = CM;
-        }
-        convertedLength = new Length(value, unit);
-        return convertedLength;
+    static Length km(double magnitude) {
+        return new Length(magnitude, Unit.KM);
+    }
+
+    static Length meter(double magnitude) {
+        return new Length(magnitude, Unit.METER);
     }
 
     @Override
@@ -47,11 +38,24 @@ class Length {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Length other = (Length) o;
-        return compareTo(other);
+        return convertToCentiMeter() == other.convertToCentiMeter();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(value, unit);
+        return Objects.hash(magnitude, unit.hashCode());
     }
+
+    private double convertToCentiMeter() {
+        switch (unit) {
+            case METER:
+                return magnitude * MULTIPLIER_CM_METER;
+            case KM:
+                return magnitude * MULTIPLIER_CM_KILOMETER;
+            default:
+                return magnitude;
+        }
+
+    }
+
 }
--- src/test/com/tw/vapasi/LengthTest.java
+++ src/test/com/tw/vapasi/LengthTest.java
@@ -2,57 +2,66 @@ package com.tw.vapasi;
 
 import org.junit.jupiter.api.Test;
 
+import static com.tw.vapasi.Length.cm;
+import static com.tw.vapasi.Length.km;
+import static com.tw.vapasi.Length.meter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class LengthTest {
     @Test
     void expectHundredCmEqualToOneMeter() {
-        Length hundredCm = new Length(100, "cm");
-        Length oneMeter = new Length(1, "m");
-        assertEquals(hundredCm, oneMeter);
+        assertEquals(cm(100), meter(1));
     }
 
     @Test
     void expectTwoHundredCmNotEqualToOneMeter() {
-        Length hundredCm = new Length(200, "cm");
-        Length oneMeter = new Length(1, "m");
-        assertNotEquals(hundredCm, oneMeter);
+        assertNotEquals(cm(200), meter(1));
     }
 
     @Test
     void expectOneMeterEqualToHundredCm() {
-        Length hundredCm = new Length(100, "cm");
-        Length oneMeter = new Length(1, "m");
-        assertEquals(oneMeter, hundredCm);
+        assertEquals(meter(1), cm(100));
     }
 
     @Test
     void expectTwoMeterNotEqualToHundredCm() {
-        Length hundredCm = new Length(100, "cm");
-        Length twoMeter = new Length(2, "m");
-        assertNotEquals(twoMeter, hundredCm);
+        assertNotEquals(meter(2), cm(100));
     }
 
     @Test
     void expectOneThousandMeterEqualToOneKiloMeter() {
-        Length thousandMeter = new Length(1000, "m");
-        Length oneKiloMeter = new Length(1, "km");
-        assertEquals(thousandMeter, oneKiloMeter);
+        assertEquals(meter(1000), km(1));
     }
 
     @Test
     void expectOneThousandMeterNotEqualToTwoKiloMeter() {
-        Length thousandMeter = new Length(1000, "m");
-        Length twoKiloMeter = new Length(2, "km");
-        assertNotEquals(thousandMeter, twoKiloMeter);
+        assertNotEquals(meter(1000), km(2));
     }
 
     @Test
     void expectOneKiloMeterEqualToOneLakhCm() {
-        Length oneLakhCm = new Length(100000, "cm");
-        Length oneKiloMeter = new Length(1, "km");
-        assertEquals(oneKiloMeter, oneLakhCm);
+        assertEquals(km(1), cm(100000));
+    }
+
+    @Test
+    void expectThousandMeterEqualToOneLakhCm() {
+        assertEquals(meter(1000), cm(100000));
+    }
+
+    @Test
+    void expectHundredCmNotEqualToThirtyCm() {
+        assertNotEquals(cm(100), cm(30));
+    }
+
+    @Test
+    void expectTwentyMeterNotEqualToHundredMeter() {
+        assertNotEquals(meter(20), meter(100));
+    }
+
+    @Test
+    void expectOneKilometerNotEqualToFortyKilometer() {
+        assertNotEquals(km(1), km(40));
     }
 
 }
